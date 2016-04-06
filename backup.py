38b07ed5a48b41775a6753bd13cba70eee434e13
mezhou887 <mezhou887@foxmail.com>
2016-04-06 23:14:09 +0800

    备份文件命令

---
+++ backup.py
@@ -0,0 +1,8 @@
+import commands,time
+
+if __name__ =="__main__":
+    # linux shell script： tar -zcvf cnbeta.tar.gz cnbeta
+    s = 'cnbeta'
+    command = 'tar -zcvf ' +s +  time.strftime("%Y%m%d%H%M%S", time.localtime()) +'.tar.gz ' + s
+    commands.getstatusoutput(command)
+
