2f6e9dc5d4bf85b0c1bcb2214d466eef7480f891
mezhou887 <mezhou887@foxmail.com>
2016-04-07 23:20:25 +0800

    添加单元测试框架

---
+++ test/csstest.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 2016年4月7日
+
+@author: 周茂恩
+'''
+import unittest
+
+
+class TestCssFunctions(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def testSample1(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
