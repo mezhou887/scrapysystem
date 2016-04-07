388e2cc96bdcccd402d9f851df0cf3e231623597
mezhou887 <mezhou887@foxmail.com>
2016-04-07 22:03:10 +0800

    添加xpath的单元测试

--- test/xpathtest.py
+++ test/xpathtest.py
@@ -3,6 +3,8 @@ import unittest
 
 # link1: http://www.cnblogs.com/08shiyan/archive/2013/05/02/3055078.html
 # link2: http://www.w3school.com.cn/xpath/index.asp
+# link3: http://www.cnblogs.com/ziyunfei/archive/2012/10/05/2710631.html
+# link4: http://www.cnblogs.com/think/archive/2006/10/11/525947.html
 
 
 class TestXpathFunctions(unittest.TestCase):
@@ -44,15 +46,26 @@ class TestXpathFunctions(unittest.TestCase):
         self.assertEqual(sel.xpath('//div/a[last()]/@href').extract()[0], 'image5.html')
         self.assertEqual(sel.xpath('//a[contains(text(), "image 2")]/@href').extract()[0], 'image2.html')
         
-        print '1.', sel.xpath('//div/a/@href').extract(); # [u'image1.html', u'image2.html', u'image3.html', u'image4.html', u'image5.html']
-        print '2.', sel.xpath('//a//@src').extract(); # [u'image1_thumb.jpg', u'image2_thumb.jpg', u'image3_thumb.jpg', u'image4_thumb.jpg', u'image5_thumb.jpg']
-        print '3.', sel.xpath('//div/a[position()<3]/@href').extract(); # [u'image1.html', u'image2.html']
-        print '4.', sel.xpath('//img[@src]').extract(); # [u'<img src="image1_thumb.jpg">', u'<img src="image2_thumb.jpg">', u'<img src="image3_thumb.jpg">', u'<img src="image4_thumb.jpg">', u'<img src="image5_thumb.jpg">']
-
-
+        # 1. [u'image1.html', u'image2.html', u'image3.html', u'image4.html', u'image5.html']
+        print '1.', sel.xpath('//div/a/@href').extract()
         
-if __name__ == '__main__': 
-    unittest.main()
+        # 2. [u'image1_thumb.jpg', u'image2_thumb.jpg', u'image3_thumb.jpg', u'image4_thumb.jpg', u'image5_thumb.jpg']
+        print '2.', sel.xpath('//a//@src').extract()
+        
+        # 3. [u'image1.html', u'image2.html']
+        print '3.', sel.xpath('//div/a[position()<3]/@href').extract()
+        
+        # 4. [u'<img src="image1_thumb.jpg">', u'<img src="image2_thumb.jpg">', u'<img src="image3_thumb.jpg">', u'<img src="image4_thumb.jpg">', u'<img src="image5_thumb.jpg">']
+        print '4.', sel.xpath('//img[@src]').extract()
+        
+        # 5. [u'<a href="image1.html">Name: My image 1 <br><img src="image1_thumb.jpg"></a>']
+        print '5.', sel.xpath('//div/a[@href="image1.html"]').extract()
+        
+        # 6. [u'<br>', u'<img src="image2_thumb.jpg">']
+        print '6.', sel.xpath('//a[@href="image2.html"]/*').extract()
+
+        # 7. [u'<br>', u'<br>', u'<br>', u'<br>', u'<br>']
+        print '7.', sel.xpath('//a/*[1]').extract()   
 
 
 
