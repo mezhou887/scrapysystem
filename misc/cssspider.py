e335a1204e5880f00906ce0df4e738330fb343e1
mezhou887 <mezhou887@foxmail.com>
2016-04-07 22:27:25 +0800

    精简掉注释内容，注意cssspider未做测试

--- misc/cssspider.py
+++ misc/cssspider.py
@@ -5,51 +5,9 @@ import re
 from scrapy.selector import Selector
 from scrapy.spiders import CrawlSpider
 
-
-'''
-1. 默认取sel.css()[0]，如否则需要'__unique':False or __list:True
-2. 默认字典均为css解析，如否则需要'__use':'dump'表明是用于dump数据
-'''
-
-
 class CommonSpider(CrawlSpider):
 
-    auto_join_text = True
-    ''' # css rule example:
-    all_css_rules = {
-        '.zm-profile-header': {
-            '.zm-profile-header-main': {
-                '__use':'dump',
-                'name':'.title-section .name::text',
-                'sign':'.title-section .bio::text',
-                'location':'.location.item::text',
-                'business':'.business.item::text',
-                'employment':'.employment.item::text',
-                'position':'.position.item::text',
-                'education':'.education.item::text',
-                'education_extra':'.education-extra.item::text',
-            }, '.zm-profile-header-operation': {
-                '__use':'dump',
-                'agree':'.zm-profile-header-user-agree strong::text',
-                'thanks':'.zm-profile-header-user-thanks strong::text',
-            }, '.profile-navbar': {
-                '__use':'dump',
-                'asks':'a[href*=asks] .num::text',
-                'answers':'a[href*=answers] .num::text',
-                'posts':'a[href*=posts] .num::text',
-                'collections':'a[href*=collections] .num::text',
-                'logs':'a[href*=logs] .num::text',
-            },
-        }, '.zm-profile-side-following': {
-            '__use':'dump',
-            'followees':'a.item[href*=followees] strong::text',
-            'followers':'a.item[href*=followers] strong::text',
-        }
-    }
-    '''
-
-    # Extract content without any extra spaces.
-    # NOTE: If content only has spaces, then it would be ignored.
+    keywords = set(['__use', '__list'])
     def extract_item(self, sels):
         contents = []
         for i in sels:
@@ -65,16 +23,11 @@ class CommonSpider(CrawlSpider):
             if nk not in item:
                 item[nk] = []
             if sel.css(nv):
-                # item[nk] += [i.extract() for i in sel.css(nv)]
-                # Without any extra spaces:
                 item[nk] += self.extract_item(sel.css(nv))
             else:
                 item[nk] = []
 
-    # 1. item是一个单独的item，所有数据都聚合到其中 *merge
-    # 2. 存在item列表，所有item归入items
     def traversal(self, sel, rules, item_class, item, items):
-        # print 'traversal:', sel, rules.keys()
         if item is None:
             item = item_class()
         if '__use' in rules:
@@ -90,7 +43,7 @@ class CommonSpider(CrawlSpider):
                     self.traversal(i, nv, item_class, item, items)
 
     def deal_text(self, sel, item, force_1_item, k, v):
-        if v.endswith('::text') and self.auto_join_text:
+        if v.endswith('::text'):
             item[k] = ' '.join(self.extract_item(sel.css(v)))
         else:
             _items = self.extract_item(sel.css(v))
@@ -102,9 +55,7 @@ class CommonSpider(CrawlSpider):
             else:
                 item[k] = _items
 
-    keywords = set(['__use', '__list'])
     def traversal_dict(self, sel, rules, item_class, item, items, force_1_item):
-        #import pdb; pdb.set_trace()
         item = {}
         for k, v in rules.items():
             if type(v) != dict:
@@ -113,11 +64,9 @@ class CommonSpider(CrawlSpider):
                 if type(v) == list:
                     continue
                 self.deal_text(sel, item, force_1_item, k, v)
-                #import pdb;pdb.set_trace()
             else:
                 item[k] = []
                 for i in sel.css(k):
-                    #print(k, v)
                     self.traversal_dict(i, v, item_class, item, item[k], force_1_item)
         items.append(item)
 
@@ -130,16 +79,7 @@ class CommonSpider(CrawlSpider):
             self.traversal(sel, rules, item_class, None, items)
         else:
             self.traversal_dict(sel, rules, item_class, None, items, force_1_item)
-
         return items
 
     def parse_with_rules(self, response, rules, item_class, force_1_item=False):
         return self.dfs(Selector(response), rules, item_class, force_1_item)
-
-    ''' # use parse_with_rules example:
-    def parse_people_with_rules(self, response):
-        item = self.parse_with_rules(response, self.all_css_rules, ZhihuPeopleItem)
-        item['id'] = urlparse(response.url).path.split('/')[-1]
-        info('Parsed '+response.url) # +' to '+str(item))
-        return item
-    '''
