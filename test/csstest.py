9c3f5c0a3852557d76cfddead780448fc1bee93b
mezhou887 <mezhou887@foxmail.com>
2016-04-09 18:45:44 +0800

    完善css测试用例

--- test/csstest.py
+++ test/csstest.py
@@ -11,8 +11,30 @@ import requests
 
 class TestCssFunctions(unittest.TestCase):
 
+    sample1 = '''
+    <html>
+        <head>
+            <base href='http://example.com/' />
+            <title>Example website</title>
+        </head>
+        <body>
+            <div id='images'>
+                <a href='image1.html'>Name: My image 1 <br /><img src='image1_thumb.jpg' /></a>
+                <a href='image2.html'>Name: My image 2 <br /><img src='image2_thumb.jpg' /></a>
+                <a href='imageA.html'>Name: My image 3 <br /><img src='image3_thumb.jpg' /></a>
+                <a href='image4.html'>Name: My image 4 <br /><img src='image4_thumb.jpg' /></a>
+                <a href='image5.html'>Name: My image 5 <br /><img src='image5_thumb.jpg' /></a>
+            </div>
+        </body>
+    </html>
+    '''    
+
     def testSample1(self):
-        pass
+        sel = Selector(text=self.sample1);
+        print '1.', sel.css('img').xpath('@src').extract()
+        print '2.', sel.css('base::attr(href)').extract()
+        print '3.', sel.css('a[href*=image]::attr(href)').extract()
+        print '4.', sel.css('a[href*=image] img::attr(src)').extract()
     
     def testSample2(self):
         str = 'http://doc.scrapy.org/en/latest/_static/selectors-sample1.html'
