eea7522b55f5cbda40f5e629a8f3b03a1e4e46c3
mezhou887 <mezhou887@foxmail.com>
2016-04-09 18:34:31 +0800

    更新停止运行项目的脚本

--- doc/stopScrapy.sh
+++ doc/stopScrapy.sh
@@ -1,1 +1,4 @@
 /Users/zhoumaoen/redis/bin/redis-cli shutdown  &
+kill `ps -ef | grep mongodb | grep -v grep | awk '{print $2}'` &
+kill `ps -ef | grep redis | grep -v grep | awk '{print $2}'` &
+kill `ps -ef | grep scrapyd | grep -v grep | awk '{print $2}'` &
