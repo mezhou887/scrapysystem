c2d04a8d5d099b31051d4a88f62297577a4d5b77
mezhou887 <mezhou887@foxmail.com>
2016-04-10 15:50:42 +0800

    regex单元测试

--- test/regextest.py
+++ test/regextest.py
@@ -7,10 +7,18 @@ Created on 2016年4月7日
 import unittest
 import re
 
+# link1: http://blog.jobbole.com/74844/
+
 class TestRegexFunctions(unittest.TestCase):
 
     def testSample1(self):
         print re.match("c", "abcdef")
+        
+    def testSample2(self):
+        pattern = re.compile(r'world')
+        match = pattern.search('hello world!')
+        if match:
+            print match.group()
 
 if __name__ == "__main__":
     unittest.main()
