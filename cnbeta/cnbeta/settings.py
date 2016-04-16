478084ff357c7d759e1c0093e41087848bac72fb
mezhou887 <mezhou887@foxmail.com>
2016-04-16 11:34:59 +0800

    更新pipeline部分

--- cnbeta/cnbeta/settings.py
+++ cnbeta/cnbeta/settings.py
@@ -1,5 +1,7 @@
 # -*- coding:utf-8 -*-
+import os
 
+PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
 
 BOT_NAME = 'cnbeta'
 
@@ -7,20 +9,18 @@ SPIDER_MODULES = ['cnbeta.spiders']
 NEWSPIDER_MODULE = 'cnbeta.spiders'
 
 DOWNLOADER_MIDDLEWARES = {
-   # 'misc.middleware.CustomHttpProxyMiddleware': 400,
-   # 'misc.middleware.CustomHttpsProxyMiddleware': 400,
-   # 'misc.middleware.CustomUserAgentMiddleware': 400,
+   # 'misc.middleware.CustomHttpProxyMiddleware': 100,
+   # 'misc.middleware.CustomHttpsProxyMiddleware': 200,
+   # 'misc.middleware.CustomUserAgentMiddleware': 300,
 }
 
 ITEM_PIPELINES = {
-   #'cnbeta.pipelines.DoNothingPipeline': 300,
-  'cnbeta.pipelines.ImageDownloadPipeline': 200,
-  'cnbeta.pipelines.JsonWithEncodingPipeline': 300,
-   #'cnbeta.pipelines.CsvPipeline': 300,  
-   #'cnbeta.pipelines.MongoDBPipeline': 300,   
-   #'cnbeta.pipelines.MySQLPipeline': 300,    
-   #'cnbeta.pipelines.RedisPipeline': 300,
-   'scrapy_redis.pipelines.RedisPipeline': 400,   
+    'cnbeta.file_pipeline.DoNothingPipeline': 100,
+    'scrapy.pipelines.images.ImagesPipeline': 200,
+    'cnbeta.file_pipeline.JsonPipeline': 300,
+    #'cnbeta.mysql_pipeline.MySQLPipeline': 400,    
+    #'cnbeta.redis_pipeline.RedisPipeline': 500,   
+    #'scrapy_mongodb.MongoDBPipeline': 600,
 }
 
 #Log Info
@@ -28,12 +28,31 @@ LOG_FILE = 'cnbeta.log'
 LOG_LEVEL = 'INFO'
 #LOG_LEVEL = 'DEBUG'
 
-#File Store 
-IMAGES_STORE = '/Users/zhoumaoen/data/images'
+#Image Store 
+IMAGES_STORE = os.path.join(PROJECT_DIR,'data/images')
+IMAGES_EXPIRES = 30
+IMAGES_THUMBS = {
+    'small': (50, 50),
+    'big': (270, 270),
+}
 
 #Redis Config 使用redis打开，不用redis请注释掉
+# https://github.com/rolando/scrapy-redis
 SCHEDULER = "scrapy_redis.scheduler.Scheduler"
 SCHEDULER_PERSIST = True
+SCHEDULER_QUEUE_CLASS = 'scrapy_redis.queue.SpiderPriorityQueue'
+
+# Mongodb Config
+# http://sebdah.github.io/scrapy-mongodb/
+MONGODB_URI = 'mongodb://localhost:27017'
+MONGODB_DATABASE = 'scrapy'
+MONGODB_COLLECTION = 'cnbeta_mongo'
+
+#MySQL Config
+MYSQL_HOST = 'localhost'
+MYSQL_DBNAME = 'scrapy'
+MYSQL_USER = 'mezhou887'
+MYSQL_PASSWD = '123456'
 
 DOWNLOAD_DELAY = 1
 DOWNLOAD_TIMEOUT = 10
