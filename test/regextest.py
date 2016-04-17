9c3f24cd7f7f3ec1b2a241c294c22bbd1b31b0d7
mezhou887 <mezhou887@foxmail.com>
2016-04-17 16:06:15 +0800

    添加正则表达式范例

--- test/regextest.py
+++ test/regextest.py
@@ -19,6 +19,20 @@ class TestRegexFunctions(unittest.TestCase):
         match = pattern.search('hello world!')
         if match:
             print match.group()
+            
+    def testSample3(self):
+        text = '123pythontab123AAA2BBB'
+        m = re.search(r"\d+", text)
+        if m: 
+            print m.group(0)
+        else:
+            print 'not match'
+            
+    def testSample4(self):
+        text = '123pythontab1423AAA2BBB'
+        p = re.compile("\d+")
+        print p.findall(text)
+        print re.findall(r'[a-zA-Z]+', text)
 
 if __name__ == "__main__":
     unittest.main()
