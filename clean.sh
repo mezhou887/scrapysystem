1412d25565a3d13216deef5c8710593ffdbb2de4
mezhou887 <mezhou887@foxmail.com>
2016-04-17 09:58:15 +0800

    bugfix

--- clean.sh
+++ clean.sh
@@ -1,4 +1,4 @@
 find . -name '*.pyc' | xargs rm
 find . -name '.DS_Store' | xargs rm
-find . -name '.log' | xargs rm
-find . -name '.json' | xargs rm
+find . -name '*.log' | xargs rm
+find . -name '*.json' | xargs rm
