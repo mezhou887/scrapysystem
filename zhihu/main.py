4e05f052ad0a5aa213e7a66172b447500b12d95c
mezhou887 <mezhou887@foxmail.com>
2016-05-03 08:26:16 +0800

    初步

--- zhihu/main.py
+++ zhihu/main.py
@@ -5,7 +5,6 @@ from scrapy import cmdline
 if __name__ =="__main__":
     
 #     cmdline.execute("scrapy crawl zhihu_login".split());
-#     cmdline.execute("scrapy crawl zhihu_user".split());
-    cmdline.execute("scrapy crawl zhihu_user_test".split());
+    cmdline.execute("scrapy crawl zhihu_user".split());
 #     cmdline.execute("scrapy crawl zhihu_ask".split());
 #     cmdline.execute("scrapy crawl zhihu_answer".split());
