448ec3a5ba5f955f2e829c81d800ccc51ba40d19
mezhou887 <mezhou887@foxmail.com>
2016-05-20 21:13:57 +0800

    网速不行，明天测试

--- luoo/luoo/file_pipeline.py
+++ luoo/luoo/file_pipeline.py
@@ -25,30 +25,20 @@ class JsonPipeline(object):
 class CustomerFilesPipeline(FilesPipeline):
     
     def __init__(self, store_uri, download_func=None):
-        super(CustomerFilesPipeline, self).__init__(store_uri, download_func)    
         self.base = store_uri
+        super(CustomerFilesPipeline, self).__init__(store_uri, download_func)    
         
     def get_media_requests(self, item, info):
         self.title = self.base + '/' + item['title']
-        print self.title
         self.name = item['musicname']
         return [Request(item.get(self.FILES_URLS_FIELD))] 
     
     def file_path(self, request, response=None, info=None):
-        if not isinstance(request, Request):
-            url = request
-        else:
-            url = request.url
-
-        if not hasattr(self.file_key, '_base'):
-            return self.file_key(url)
-        
         folder = self.title
         if not os.path.exists(folder):
             os.mkdir(folder)        
         
-        media_guid = self.name; 
-        media_ext = os.path.splitext(url)[1]
-        return '%s/%s%s' % (folder, media_guid, media_ext)   
+        media_ext = os.path.splitext(request.url)[1]
+        return '%s/%s%s' % (folder, self.name, media_ext)   
     
         
