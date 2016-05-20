92353474cc3befc2256e46589a91127d51f4d038
mezhou887 <mezhou887@foxmail.com>
2016-05-20 12:45:01 +0800

    完成开发

--- luoo/luoo/file_pipeline.py
+++ luoo/luoo/file_pipeline.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import json
+import os
 import codecs
 from collections import OrderedDict
 from luoo.items import *
@@ -23,7 +24,31 @@ class JsonPipeline(object):
         
 class CustomerFilesPipeline(FilesPipeline):
     
+    def __init__(self, store_uri, download_func=None):
+        super(CustomerFilesPipeline, self).__init__(store_uri, download_func)    
+        self.base = store_uri
+        
     def get_media_requests(self, item, info):
-        return [Request(item.get(self.FILES_URLS_FIELD))]    
+        self.title = self.base + '/' + item['title']
+        print self.title
+        self.name = item['musicname']
+        return [Request(item.get(self.FILES_URLS_FIELD))] 
+    
+    def file_path(self, request, response=None, info=None):
+        if not isinstance(request, Request):
+            url = request
+        else:
+            url = request.url
+
+        if not hasattr(self.file_key, '_base'):
+            return self.file_key(url)
+        
+        folder = self.title
+        if not os.path.exists(folder):
+            os.mkdir(folder)        
+        
+        media_guid = self.name; 
+        media_ext = os.path.splitext(url)[1]
+        return '%s/%s%s' % (folder, media_guid, media_ext)   
     
         
